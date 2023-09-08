d742a4049daccc065c8bba953df3b91f86e9d07c
Andoniank <andoniank@gmail.com>
2023-09-07 23:58:39 -0700

    Finish .uniq

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,14 @@
 // Your code here
+Array.prototype.uniq = function() {
+    let arr = []
+
+    for (let i = 0; i < this.length; i++) {
+        if (arr.indexOf(this[i]) === -1) {
+            arr.push(this[i])
+        }
+    }
+
+    return arr
+}
+
+console.log([1,2,2,3,3,3].uniq())
